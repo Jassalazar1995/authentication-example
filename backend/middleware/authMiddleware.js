53db8683146956d6321ecd042a67124a69642dcf
Jose Salazar <jsalazar@pneumarespiratory.com>
2024-01-22 11:55:09 -0500

    finished steps in the middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken')
+
 function authorize(req,res, next){
     
 try {
@@ -10,14 +12,25 @@ try {
     if(!token){
         return res.status(403).json({ error: "No token provided" })
     }
-    console.log(token)
+    console.log(token) // "Bearer 9j343jfdsf94FDSAf43"
+
+    token = token.replace("Bearer ", "") // "9j343jfdsf94FDSAf43"
+
     // 2. Check that the token is valid and not expired
+
+    const payload = jwt.verify(token,process.env.JWT_SECRET)
+
+    if (payload.error){
+        return res.status(400).json({ error: payload.error })
+
+    }
 
     
 
-
     // 3. Attach the payload from the token to the request object (req)
-
+    req.id = payload.id
+    req.username = payload.username
+    
     // 4. Move on to the requested route (next)
 
     next()
